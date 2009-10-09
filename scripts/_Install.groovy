18e8f9604538417ff93fe1d9daa6a02a21fffd92
aaron <aaron@aaron.(none)>
2009-10-09 10:52:38 -0500

    upgrading development version to grails 1.1.1

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,14 +1,14 @@
 
 Ant.mkdir(dir:"${basedir}/grails-app/views/pdf")
-Ant.copy(file:"${pluginBasedir}/grails-app/views/demo.gsp",
+Ant.copy(file:"${pluginBasedir}/grails-app/views/pdf/demo.gsp",
          todir:"${basedir}/grails-app/views/pdf")
-Ant.copy(file:"${pluginBasedir}/grails-app/views/demo2.gsp",
+Ant.copy(file:"${pluginBasedir}/grails-app/views/pdf/demo2.gsp",
          todir:"${basedir}/grails-app/views/pdf")
-Ant.copy(file:"${pluginBasedir}/grails-app/views/_demo2.gsp",
+Ant.copy(file:"${pluginBasedir}/grails-app/views/pdf/_demo2.gsp",
 		 todir:"${basedir}/grails-app/views/pdf")	
-Ant.copy(file:"${pluginBasedir}/grails-app/views/demo3.gsp",
+Ant.copy(file:"${pluginBasedir}/grails-app/views/pdf/demo3.gsp",
          todir:"${basedir}/grails-app/views/pdf")
-Ant.copy(file:"${pluginBasedir}/grails-app/views/sampleInclude.gsp",
+Ant.copy(file:"${pluginBasedir}/grails-app/views/pdf/sampleInclude.gsp",
          todir:"${basedir}/grails-app/views/pdf")
          
 //this script installs the demo files for the PDF plugin. The file and the directory can be 
